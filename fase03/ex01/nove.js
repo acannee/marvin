cf91ffd7290927cba3b568b12ffde620b91e5b30
acannee <57539101+acannee@users.noreply.github.com>
2019-12-06 08:52:56 -0300

    I9

--- fase03/ex01/nove.js
+++ fase03/ex01/nove.js
@@ -1,14 +1,4 @@
 function nove(num){
-x=num.toString();
-resto=x.charAt(x.length-1);
-if (resto==9){
-return("true")
-} 
-else{
-    return("false");
+const numString = num.toString();
+return numString[numString.length - 1] === "9";
 }
-
-}
-
-
-
